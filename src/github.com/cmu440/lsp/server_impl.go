79a4afe9ede883c59fc97e0c908968c65b67c15e
Zhengze Gong <zhengzeg@andrew.cmu.edu>
2020-09-30 00:54:16 +0800

    reformat file

--- src/github.com/cmu440/lsp/server_impl.go
+++ src/github.com/cmu440/lsp/server_impl.go
@@ -3,11 +3,11 @@
 package lsp
 
 import (
+	"encoding/json"
 	"errors"
 	"github.com/cmu440/lspnet"
-	"encoding/json"
-	"strconv"
 	"sort"
+	"strconv"
 )
 
 type server struct {
@@ -18,24 +18,24 @@ type server struct {
 	counter int // counter for creating connID
 	// Messages
 	orderedData []*Message
-	readChan chan *Message
-	writeChan chan *writeMsg
+	readChan    chan *Message
+	writeChan   chan *writeMsg
 }
 
-// Stores all information of a client, defined when a connection is 
+// Stores all information of a client, defined when a connection is
 // established
 type clientInfo struct {
-	connID int
-	cliAddr *lspnet.UDPAddr
-	writeSeqNum int // Latest Sequence Number for writes
-	readSeqNum int // Latest Sequence Number (ordered) for reads
-	pendingData []*Message // Incoming unordered msgs
+	connID        int
+	cliAddr       *lspnet.UDPAddr
+	writeSeqNum   int        // Latest Sequence Number for writes
+	readSeqNum    int        // Latest Sequence Number (ordered) for reads
+	pendingData   []*Message // Incoming unordered msgs
 	pendingWrites []*Message // Unsent msgs (due to window size, etc)
 	unackedWrites []*Message // Sent msgs but not acked by client
 }
 
 type writeMsg struct {
-	connID int
+	connID  int
 	payload []byte
 	retChan chan error
 }
@@ -47,221 +47,221 @@ type writeMsg struct {
 // project 0, etc.) and immediately return. It should return a non-nil error if
 // there was an error resolving or listening on the specified port number.
 func NewServer(port int, params *Params) (Server, error) {
-	addr, resErr := lspnet.ResolveUDPAddr("udp", ":" + strconv.Itoa(port))
+	addr, resErr := lspnet.ResolveUDPAddr("udp", ":"+strconv.Itoa(port))
 	if resErr != nil {
-	    return nil, resErr
+		return nil, resErr
 	}
 	serverConn, listErr := lspnet.ListenUDP("udp", addr)
 	if listErr != nil {
-	    return nil, listErr
+		return nil, listErr
 	}
-	
+
 	s := &server{
-	    udpAddr:        addr,
-	    udpConn:        serverConn,
-	    clients:        nil,
-	    counter:        0,
-	    orderedData:    nil,
-	    readChan:       make(chan *Message, 20),
-	    writeChan:      make(chan *writeMsg),
+		udpAddr:     addr,
+		udpConn:     serverConn,
+		clients:     nil,
+		counter:     0,
+		orderedData: nil,
+		readChan:    make(chan *Message, 20),
+		writeChan:   make(chan *writeMsg),
 	}
-	
+
 	go s.MainRoutine()
 	go s.ReadRoutine()
-	
+
 	return s, nil
 }
 
 // Processes all incoming messages
 func (s *server) MainRoutine() {
 	for {
-	    select {
-		case req  := <-s.writeChan:
-	        c := s.findClient(req.connID)
-	        if c == nil {
-	            req.retChan<- errors.New("client connection has been closed")
-	            break
-	        }
-	        req.retChan<- nil
-	
-	        cs := s.CalculateCheckSum(
-	            c.connID, c.writeSeqNum + 1,len(req.payload), req.payload,
-	        )
-	        msg := NewData(
-	            c.connID, c.writeSeqNum + 1, len(req.payload), 
-	            req.payload, cs,
-	        )
-	        c.writeSeqNum++
-	        s.writeToClient(c, msg)
-	    }
+		select {
+		case req := <-s.writeChan:
+			c := s.findClient(req.connID)
+			if c == nil {
+				req.retChan <- errors.New("client connection has been closed")
+				break
+			}
+			req.retChan <- nil
+
+			cs := s.CalculateCheckSum(
+				c.connID, c.writeSeqNum+1, len(req.payload), req.payload,
+			)
+			msg := NewData(
+				c.connID, c.writeSeqNum+1, len(req.payload),
+				req.payload, cs,
+			)
+			c.writeSeqNum++
+			s.writeToClient(c, msg)
+		}
 	}
 }
 
-// Reads from udp connection; determines the msg type and sends msg to 
+// Reads from udp connection; determines the msg type and sends msg to
 // mainroutine for further processing
 func (s *server) ReadRoutine() {
-    for {
-        // Read from connection
-        buff := make([]byte, 2000)
-        len, cliAddr, err := s.udpConn.ReadFromUDP(buff[0:])
-        if err != nil {
-            return
-        }
-        
-        // Parse message
-        var msg Message
-        if json.Unmarshal(buff[0:len], &msg) != nil {
-            continue
-        }
-        // Check message type
-        switch msg.Type {
-            case MsgConnect:
-                addr := cliAddr
-                c := s.addClient(addr)
-                // send ack msg
-                ack := NewAck(c.connID, 0)
-                s.writeToClient(c, ack)
+	for {
+		// Read from connection
+		buff := make([]byte, 2000)
+		len, cliAddr, err := s.udpConn.ReadFromUDP(buff[0:])
+		if err != nil {
+			return
+		}
 
-            case MsgData:
-                c := s.findClient(msg.ConnID)
-                if c == nil {
-                    break
-                }
-                s.storeData(c, &msg)
-                // send ack msg
-                ack := NewAck(c.connID, msg.SeqNum)
-                s.writeToClient(c, ack)
+		// Parse message
+		var msg Message
+		if json.Unmarshal(buff[0:len], &msg) != nil {
+			continue
+		}
+		// Check message type
+		switch msg.Type {
+		case MsgConnect:
+			addr := cliAddr
+			c := s.addClient(addr)
+			// send ack msg
+			ack := NewAck(c.connID, 0)
+			s.writeToClient(c, ack)
 
-            case MsgAck:
-            // TODO: implement sliding window
-        }
-    }
+		case MsgData:
+			c := s.findClient(msg.ConnID)
+			if c == nil {
+				break
+			}
+			s.storeData(c, &msg)
+			// send ack msg
+			ack := NewAck(c.connID, msg.SeqNum)
+			s.writeToClient(c, ack)
+
+		case MsgAck:
+			// TODO: implement sliding window
+		}
+	}
 }
 
 func (s *server) Read() (int, []byte, error) {
-    if s.readChan == nil {
-        return 0, nil, errors.New("The server has been closed")
-    }
-    msg := <-s.readChan
-    return msg.ConnID, msg.Payload, nil
+	if s.readChan == nil {
+		return 0, nil, errors.New("The server has been closed")
+	}
+	msg := <-s.readChan
+	return msg.ConnID, msg.Payload, nil
 }
 
 func (s *server) Write(connId int, payload []byte) error {
-    errChan := make(chan error)
-    s.writeChan<- &writeMsg {
-        connID: connId,
-        payload: payload,
-        retChan: errChan,
-    }
+	errChan := make(chan error)
+	s.writeChan <- &writeMsg{
+		connID:  connId,
+		payload: payload,
+		retChan: errChan,
+	}
 
-    return <-errChan
+	return <-errChan
 }
 
 func (s *server) CloseConn(connId int) error {
-    return errors.New("not yet implemented")
+	return errors.New("not yet implemented")
 }
 
 func (s *server) Close() error {
-    return errors.New("not yet implemented")
+	return errors.New("not yet implemented")
 }
 
 // ============================= Helper Functions =============================
 // Creates a new client and add to server's list; returns the new client
 func (s *server) addClient(addr *lspnet.UDPAddr) *clientInfo {
-    c := &clientInfo{
-        connID: s.counter + 1,
-        cliAddr: addr,
-        writeSeqNum: 0,
-        readSeqNum: 0,
-    }
+	c := &clientInfo{
+		connID:      s.counter + 1,
+		cliAddr:     addr,
+		writeSeqNum: 0,
+		readSeqNum:  0,
+	}
 
-    s.clients = append(s.clients, c)
-    s.counter++
-    return c
+	s.clients = append(s.clients, c)
+	s.counter++
+	return c
 }
 
 func (s *server) findClient(connid int) *clientInfo {
-    for _, c := range s.clients {
-        if c.connID == connid {
-            return c
-        }
-    }
-    return nil
+	for _, c := range s.clients {
+		if c.connID == connid {
+			return c
+		}
+	}
+	return nil
 }
 
 func (s *server) removeClient(connid int) *clientInfo {
-    for i, c := range s.clients {
-        if c.connID == connid {
-            s.clients[i] = s.clients[len(s.clients) - 1]
-            return c
-        }
-    }
-    return nil
+	for i, c := range s.clients {
+		if c.connID == connid {
+			s.clients[i] = s.clients[len(s.clients)-1]
+			return c
+		}
+	}
+	return nil
 }
 
 func (s *server) writeToClient(c *clientInfo, msg *Message) error {
-    b, marshalErr := json.Marshal(msg)
-    if marshalErr != nil {
-        return marshalErr
-    }
+	b, marshalErr := json.Marshal(msg)
+	if marshalErr != nil {
+		return marshalErr
+	}
 
-    _, writeErr := s.udpConn.WriteToUDP(b, c.cliAddr)
-    if writeErr != nil {
-        return writeErr
-    }
-    return nil
+	_, writeErr := s.udpConn.WriteToUDP(b, c.cliAddr)
+	if writeErr != nil {
+		return writeErr
+	}
+	return nil
 }
 
 // Stores input data msg in server if it is in order, otherwise stores in client,
 // checks for remaining client stored msgs in client and move now ordered msgs to
 // server
 func (s *server) storeData(c *clientInfo, msg *Message) {
-    // Msg in order, store in the list
-    if msg.SeqNum == c.readSeqNum + 1 {
-        s.orderedData = append(s.orderedData, msg)
-        c.readSeqNum++
-        
-        var newData []*Message // stores remaining unordered msgs
-        for _, d := range c.pendingData {
-            if d.SeqNum == c.readSeqNum + 1 {
-                s.orderedData = append(s.orderedData, d)
-                c.readSeqNum++
+	// Msg in order, store in the list
+	if msg.SeqNum == c.readSeqNum+1 {
+		s.orderedData = append(s.orderedData, msg)
+		c.readSeqNum++
 
-            } else {
-                newData = append(newData, d)
-            }
-        }
-        c.pendingData = newData
-    } else {
-        c.pendingData = append(c.pendingData, msg)      
-    }
-    
-    // sort unordered msgs
-    sort.Slice(c.pendingData, func(i, j int) bool {
-        return c.pendingData[i].SeqNum < c.pendingData[j].SeqNum
-    })
+		var newData []*Message // stores remaining unordered msgs
+		for _, d := range c.pendingData {
+			if d.SeqNum == c.readSeqNum+1 {
+				s.orderedData = append(s.orderedData, d)
+				c.readSeqNum++
 
-    data := s.orderedData
-    s.orderedData = nil
-    for _, d := range data {
-        s.readChan <- d
-    }
-    return
+			} else {
+				newData = append(newData, d)
+			}
+		}
+		c.pendingData = newData
+	} else {
+		c.pendingData = append(c.pendingData, msg)
+	}
+
+	// sort unordered msgs
+	sort.Slice(c.pendingData, func(i, j int) bool {
+		return c.pendingData[i].SeqNum < c.pendingData[j].SeqNum
+	})
+
+	data := s.orderedData
+	s.orderedData = nil
+	for _, d := range data {
+		s.readChan <- d
+	}
+	return
 }
 
 func (s *server) CalculateCheckSum(ID int, seqNum int, size int, payload []byte) uint16 {
-    var checksumTmp uint32
-    var mask uint32
-    mask = 0x0000ffff
-    checksumTmp = 0
-    checksumTmp += Int2Checksum(ID)
-    checksumTmp += Int2Checksum(seqNum)
-    checksumTmp += Int2Checksum(size)
-    checksumTmp += ByteArray2Checksum(payload)
-    for checksumTmp > 0xffff {
-     curSum := checksumTmp >> 16
-     remain := checksumTmp & mask
-     checksumTmp = curSum + remain
-    }
-    return uint16(checksumTmp)
+	var checksumTmp uint32
+	var mask uint32
+	mask = 0x0000ffff
+	checksumTmp = 0
+	checksumTmp += Int2Checksum(ID)
+	checksumTmp += Int2Checksum(seqNum)
+	checksumTmp += Int2Checksum(size)
+	checksumTmp += ByteArray2Checksum(payload)
+	for checksumTmp > 0xffff {
+		curSum := checksumTmp >> 16
+		remain := checksumTmp & mask
+		checksumTmp = curSum + remain
+	}
+	return uint16(checksumTmp)
 }
