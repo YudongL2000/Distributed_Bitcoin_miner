59d4752763927986dc144f107dced677f1903df9
YudongL2000 <yudongl@andrew.cmu.edu>
2020-10-10 14:18:23 -0400

    client_imp 2 modified

--- src/github.com/cmu440/lsp/client_impl.go
+++ src/github.com/cmu440/lsp/client_impl.go
@@ -6,7 +6,7 @@ import (
 	//"bytes"
 	"encoding/json"
 	"errors"
-	//"fmt"
+	"fmt"
 	"github.com/cmu440/lspnet"
 	"time"
 )
@@ -14,11 +14,12 @@ import (
 const MaxMessgeByteLen = 2000
 
 type sentQueueElem struct {
+	acked       bool
 	seqNum      int
 	backOff     int
 	epochPassed int
 	msg         *Message
-	sentSuccess chan bool
+	//sentSuccess chan bool
 }
 
 type client struct {
@@ -35,19 +36,25 @@ type client struct {
 	windowUnAcked     int
 	unsentBuffer      []*sentQueueElem
 	receivedMsg       []*Message
-	incomeAck         chan *Message
+	connMain          chan int
 	outgoPayload      chan []byte
+	newWindowElem     chan *sentQueueElem
+	newAckSeqNum      chan int
 	writeReq          chan bool
 	writeReturn       chan bool
 	incomeData        chan *Message
 	alreadyDisconnect bool
 	readRountineQuit  chan bool
 	mainQuit          chan bool
-	wantQuit          bool
+	quitReqTime       chan bool
+	quitConfirm       chan bool
 	disconnect        chan bool
 	timerDrop         chan bool
 	closeFinish       chan bool
 	receiveSth        chan bool
+	ackToSend         chan *Message
+	connIDReq         chan bool
+	connIDAnswer      chan int
 }
 
 // NewClient creates, initiates, and returns a new client. This function
@@ -83,37 +90,32 @@ func NewClient(hostport string, params *Params) (Client, error) {
 		windowUnAcked:     0,
 		unsentBuffer:      nil,
 		receivedMsg:       nil,
-		incomeAck:         make(chan *Message),
+		connMain:          make(chan int),
 		outgoPayload:      make(chan []byte),
+		newWindowElem:     make(chan *sentQueueElem),
+		newAckSeqNum:      make(chan int),
 		writeReq:          make(chan bool),
 		writeReturn:       make(chan bool),
 		incomeData:        make(chan *Message),
 		alreadyDisconnect: false,
 		readRountineQuit:  make(chan bool),
 		mainQuit:          make(chan bool),
-		wantQuit:          false,
+		quitReqTime:       make(chan bool),
+		quitConfirm:       make(chan bool),
 		disconnect:        make(chan bool),
 		timerDrop:         make(chan bool),
 		closeFinish:       make(chan bool),
 		receiveSth:        make(chan bool),
+		ackToSend:         make(chan *Message),
+		connIDReq:         make(chan bool),
+		connIDAnswer:      make(chan int),
 	}
 
 	go c.ReadRoutine()
 	go c.MainRoutine()
-	go c.HeartBeat()
-	connectedMsg := NewConnect()
-	newElem := &sentQueueElem{
-		seqNum:      0,
-		backOff:     0,
-		epochPassed: 0,
-		msg:         connectedMsg,
-		sentSuccess: make(chan bool),
-	}
-	go c.WindowElemRountine(newElem)
+	go c.TimeRoutine()
 	connectedInfo := <-c.connectionAck
-	newElem.sentSuccess <- true
 	if connectedInfo == false {
-		/*time out warning*/
 		c.Close()
 		return nil, errors.New("connection failed")
 	}
@@ -122,7 +124,9 @@ func NewClient(hostport string, params *Params) (Client, error) {
 }
 
 func (c *client) ConnID() int {
-	return c.connID
+	c.connIDReq <- true
+	answer := <- c.connIDAnswer
+	return answer
 }
 
 func (c *client) Read() ([]byte, error) {
@@ -131,11 +135,55 @@ func (c *client) Read() ([]byte, error) {
 	if msg == nil {
 		return nil, errors.New("read request denied, connection failed")
 	}
-	c.wantedMsg += 1
 	return msg.Payload, nil
 }
 
-//unimplemented
+func (c *client) Write(payload []byte) error {
+	c.writeReq <- true
+	clientStatus := <-c.writeReturn
+	if clientStatus {
+		c.outgoPayload <- payload
+		return nil
+	} else {
+		return errors.New("write Request Denied: client disconnected")
+	}
+}
+
+func (c *client) Close() error {
+	c.mainQuit <- true
+	<-c.closeFinish
+	return nil
+}
+
+//================================================= End of api Functions ====================================
+
+
+func (c *client) SendConnAck() {
+	msg := NewAck(c.connID, 0)
+	c.WriteMsg(msg)
+}
+
+func minHelper(a int, b int) int {
+	if a >= b {
+		return b
+	} else {
+		return a
+	}
+}
+
+func (c *client) WriteMsg(msg *Message) error {
+	content, err := json.Marshal(msg)
+	if err != nil {
+		return err
+	} else {
+		_, writeErr := c.connection.Write(content)
+		if writeErr != nil {
+			return writeErr
+		}
+		return nil
+	}
+}
+
 func (c *client) Message2CheckSum(ID int, seqNum int, size int, payload []byte) uint16 {
 	var checksumTmp uint32
 	var mask uint32
@@ -153,103 +201,6 @@ func (c *client) Message2CheckSum(ID int, seqNum int, size int, payload []byte) 
 	return uint16(checksumTmp)
 }
 
-func (c *client) Write(payload []byte) error {
-	c.writeReq <- true
-	clientStatus := <-c.writeReturn
-	if clientStatus {
-		c.outgoPayload <- payload
-		return nil
-	} else {
-		return errors.New("write Request Denied: client disconnected")
-	}
-}
-
-func (c *client) WindowElemRountine(element *sentQueueElem) {
-	//fmt.Printf("Start sending iteratively for message: %v\n",element.msg.SeqNum)
-	c.WriteMsg(element.msg)
-	timer := time.NewTicker(time.Duration(c.params.EpochMillis) * time.Millisecond)
-	for {
-		select {
-		case <-element.sentSuccess:
-			//fmt.Printf("sending Rountine ended for message: %v\n",element.seqNum)
-			return
-		case <-timer.C:
-			if element.epochPassed >= element.backOff {
-				timer = time.NewTicker(time.Duration(c.params.EpochMillis) * time.Millisecond)
-				c.WriteMsg(element.msg)
-				element.epochPassed = 0
-				if element.backOff == 0 {
-					element.backOff += 1
-				} else {
-					element.backOff = element.backOff * 2
-				}
-				if element.backOff > c.params.MaxBackOffInterval {
-					element.backOff = c.params.MaxBackOffInterval
-				}
-			} else {
-				element.epochPassed += 1
-			}
-		}
-	}
-}
-
-func (c *client) SendConnAck() {
-	msg := NewAck(c.connID, 0)
-	c.WriteMsg(msg)
-}
-
-func (c *client) HeartBeat() {
-	heartBeatTimer := time.NewTicker(time.Duration(c.params.EpochMillis) * time.Millisecond)
-	dropConnTimer := time.NewTicker(time.Duration(c.params.EpochLimit*c.params.EpochMillis) * time.Millisecond)
-	for {
-		select {
-		case <-heartBeatTimer.C:
-			heartBeatTimer = time.NewTicker(time.Duration(c.params.EpochMillis) * time.Millisecond)
-			c.SendConnAck()
-		case <-dropConnTimer.C:
-			if c.ackConn == 0 {
-				c.connectionAck <- false
-				c.ackConn = -1
-				return
-			} else {
-				c.disconnect <- true
-			}
-		case <-c.timerDrop:
-			return
-		case <-c.receiveSth:
-			heartBeatTimer = time.NewTicker(time.Duration(c.params.EpochMillis) * time.Millisecond)
-			dropConnTimer = time.NewTicker(time.Duration(c.params.EpochLimit*c.params.EpochMillis) * time.Millisecond)
-		}
-	}
-}
-
-func minHelper(a int, b int) int {
-	if a >= b {
-		return b
-	} else {
-		return a
-	}
-}
-
-func (c *client) Close() error {
-	c.mainQuit <- true
-	<-c.closeFinish
-	return nil
-}
-
-func (c *client) WriteMsg(msg *Message) error {
-	content, err := json.Marshal(msg)
-	if err != nil {
-		return err
-	} else {
-		_, writeErr := c.connection.Write(content)
-		if writeErr != nil {
-			return writeErr
-		}
-		return nil
-	}
-}
-
 func (c *client) CheckCorrect(m *Message) bool {
 	/*
 		actualLen := len(m.Payload)
@@ -265,6 +216,8 @@ func (c *client) CheckCorrect(m *Message) bool {
 	return true
 }
 
+//================================================= Start of window functions ================================
+
 func (c *client) SendAllAvailable() {
 	if len(c.unsentBuffer) == 0 {
 		return
@@ -276,9 +229,6 @@ func (c *client) SendAllAvailable() {
 		c.windowUnAcked = numSent
 		c.window = c.unsentBuffer[:numSent]
 		c.unsentBuffer = c.unsentBuffer[numSent:]
-		for _, elem := range c.window {
-			go c.WindowElemRountine(elem)
-		}
 		return
 	} else {
 		remain := minHelper(c.params.WindowSize-(firstUnsent-c.windowStart), len(c.unsentBuffer))
@@ -289,9 +239,6 @@ func (c *client) SendAllAvailable() {
 			newSent := c.unsentBuffer[:remain]
 			c.window = append(c.window, newSent...)
 			c.windowUnAcked += remain
-			for _, elem := range newSent {
-				go c.WindowElemRountine(elem)
-			}
 			c.unsentBuffer = c.unsentBuffer[remain:]
 		}
 	}
@@ -300,7 +247,7 @@ func (c *client) SendAllAvailable() {
 func (c *client) RemoveFinished() {
 	counter := 0
 	for _, elem := range c.window {
-		if elem.msg == nil {
+		if elem.acked {
 			counter += 1
 		} else {
 			break
@@ -313,6 +260,66 @@ func (c *client) RemoveFinished() {
 		c.windowStart = c.window[0].seqNum
 	}
 }
+
+func (c *client) ProcessAck(sn int) {
+	if (c.windowStart >=0) && (c.windowStart <= sn) {
+		idx := sn - c.windowStart
+		if c.window[idx].acked == false {
+			//c.window[idx].sentSuccess <- true
+			c.window[idx].acked = true
+			c.windowUnAcked = c.windowUnAcked - 1
+			if idx == 0 {
+				//fmt.Printf("removing window element with index %v\n", idx)
+				c.RemoveFinished()
+			}
+			c.SendAllAvailable()
+			//fmt.Printf("currently there're %v number of elements in window\n", len(c.window))
+		}
+	}
+}
+
+func (c *client) AddNewWindowElem(newElem *sentQueueElem) {
+	if len(c.window) == 0 {
+		c.window = append(c.window, newElem)
+		c.windowStart = newElem.seqNum
+		c.windowUnAcked = 1
+		c.WriteMsg(newElem.msg)
+		//go c.WindowElemRountine(newElem)
+	} else if (newElem.seqNum-c.window[0].seqNum < c.params.WindowSize) && (c.windowUnAcked < c.params.MaxUnackedMessages) {
+		c.window = append(c.window, newElem)
+		c.windowUnAcked += 1
+		c.WriteMsg(newElem.msg)
+		//go c.WindowElemRountine(newElem)
+	} else {
+		c.unsentBuffer = append(c.unsentBuffer, newElem)
+	}
+}
+
+func (c *client) UpdateWindow() bool {
+	sendSth := false
+	for _, element := range c.window {
+		if element.acked == false {
+			if element.epochPassed >= element.backOff {
+				c.WriteMsg(element.msg)
+				element.epochPassed = 0
+				if element.backOff == 0 {
+					element.backOff += 1
+				} else {
+					element.backOff = element.backOff * 2
+				}
+				if element.backOff > c.params.MaxBackOffInterval {
+					element.backOff = c.params.MaxBackOffInterval
+				}
+				sendSth = true
+			} else {
+				element.epochPassed += 1
+			}
+		}
+	}
+	return sendSth
+}
+
+//=========================================== End of window functions =====================================
 
 func (c *client) StoreData(msg *Message) {
 	var idx int
@@ -341,6 +348,90 @@ func (c *client) AbortAll() {
 	return
 }
 
+func (c *client) TimeRoutine() {
+	heartBeatTimer := time.NewTicker(time.Duration(c.params.EpochMillis) * time.Millisecond)
+	dropConnTimer := time.NewTicker(time.Duration(c.params.EpochLimit*c.params.EpochMillis) * time.Millisecond)
+	connStable := false
+	aliveConfirm := false
+	connConfirm := false
+	disconnected := false
+	wantQuit := false
+	connectionMsg := NewConnect()
+	c.WriteMsg(connectionMsg)
+	//=========================================== Need to set the connection message caller =====================================
+	for {
+		select {
+		case <-heartBeatTimer.C:
+			heartBeatTimer = time.NewTicker(time.Duration(c.params.EpochMillis) * time.Millisecond)
+			if disconnected {
+				continue
+			}
+			if connConfirm == false {
+				c.WriteMsg(connectionMsg)
+				continue
+			}
+			resendMsg := c.UpdateWindow()
+			aliveConfirm = resendMsg || aliveConfirm
+			if aliveConfirm == false {
+				c.SendConnAck()
+			} else {
+				aliveConfirm = false
+			}
+		case <-dropConnTimer.C:
+			if connConfirm == false {
+				c.connectionAck <- false
+				disconnected = true
+				c.disconnect <- true
+				continue
+			}
+			if connStable == false {
+				c.disconnect <- true
+				disconnected = true
+				if wantQuit {
+					c.quitConfirm <- true
+				}
+			} else {
+				dropConnTimer = time.NewTicker(time.Duration(c.params.EpochLimit*c.params.EpochMillis) * time.Millisecond)
+				connStable = false
+			}
+		case <-c.timerDrop:
+			return
+		case <-c.receiveSth:
+			connStable = true
+			//heartBeatTimer = time.NewTicker(time.Duration(c.params.EpochMillis) * time.Millisecond)
+			//dropConnTimer = time.NewTicker(time.Duration(c.params.EpochLimit*c.params.EpochMillis) * time.Millisecond)
+		case newElem := <-c.newWindowElem:
+			c.AddNewWindowElem(newElem)
+			fmt.Printf("sending new message %v\n", newElem.seqNum)
+			aliveConfirm = true
+		case sn := <-c.newAckSeqNum:
+			if sn == 0 {
+				if connConfirm == false {
+					connConfirm = true
+					c.connectionAck <- true
+				}
+				continue
+				//heart beat or ack
+			}
+			//fmt.Printf("recieve ack for window msg %v\n", sn)
+			c.ProcessAck(sn)
+			fmt.Printf("finished processing ack for window msg %v\n",sn)
+			if wantQuit && (len(c.window) == 0) && (len(c.unsentBuffer) == 0) {
+				c.quitConfirm <- true
+			}
+		case <-c.quitReqTime:
+			wantQuit = true
+			if disconnected {
+				c.quitConfirm <- true
+			} else if wantQuit && (len(c.window) == 0) && (len(c.unsentBuffer) == 0) {
+				c.quitConfirm <- true
+			}
+		case ack:= <-c.ackToSend:
+			c.WriteMsg(ack)
+		}
+	}
+}
+
 func (c *client) MainRoutine() {
 	for {
 		if len(c.receivedMsg) > 0 {
@@ -348,68 +439,30 @@ func (c *client) MainRoutine() {
 				readRes := c.receivedMsg[0]
 				c.receivedMsg = c.receivedMsg[1:]
 				c.readResponse <- readRes
+				c.wantedMsg += 1
 			} else if c.alreadyDisconnect {
 				c.readResponse <- nil
 			}
 		} else if c.alreadyDisconnect {
 			c.readResponse <- nil
 		}
-
 		select {
 		case <-c.mainQuit:
-			c.wantQuit = true
-			if c.alreadyDisconnect {
-				c.AbortAll()
-				return
-			} else if (len(c.window) == 0) && (len(c.unsentBuffer) == 0) {
-				c.AbortAll()
-				return
-			}
+			c.quitReqTime <- true
+		case <-c.quitConfirm:
+			c.alreadyDisconnect = true
+			c.AbortAll()
+			return
 		case <-c.disconnect:
 			if c.alreadyDisconnect == false {
-				/*cancel all sending rountine*/
-				for _, elem := range c.window {
-					if elem.msg != nil {
-						elem.sentSuccess <- true
-					}
-				}
 				c.alreadyDisconnect = true
-				if c.wantQuit {
-					c.AbortAll()
-					return
-				}
 			}
-		case ack := <-c.incomeAck:
-			sn := ack.SeqNum
-			if len(c.window) == 0 {
-				/*recieve heart beat and initial connection ack*/
-				if sn == 0 && c.ackConn == 0 {
-					c.ackConn = 1
-					c.connID = ack.ConnID
-					c.connectionAck <- true
-				}
-			} else if c.windowStart <= sn {
-				idx := sn - c.windowStart
-				if c.window[idx].msg != nil {
-					c.window[idx].sentSuccess <- true
-					c.window[idx].msg = nil
-					c.windowUnAcked = c.windowUnAcked - 1
-					if idx == 0 {
-						//fmt.Printf("removing window element with index %v\n", idx)
-						c.RemoveFinished()
-					}
-					c.SendAllAvailable()
-					//fmt.Printf("currently there're %v number of elements in window\n", len(c.window))
-				}
-			}
-			if c.wantQuit && (len(c.window) == 0) && (len(c.unsentBuffer) == 0) {
-				c.AbortAll()
-				c.alreadyDisconnect = true
-				return
+		case ID := <-c.connMain:
+			if c.ackConn == 0 {
+				c.connID = ID
+				c.ackConn = 1
 			}
 		case msg := <-c.incomeData:
-			ack := NewAck(msg.ConnID, msg.SeqNum)
-			c.WriteMsg(ack)
 			if msg.SeqNum < c.wantedMsg {
 				continue
 			} else {
@@ -422,29 +475,22 @@ func (c *client) MainRoutine() {
 			m := NewData(c.connID, c.seqNum, payloadSize, payload, checkSum)
 			c.seqNum += 1
 			newElem := &sentQueueElem{
+				acked:       false,
 				seqNum:      m.SeqNum,
 				backOff:     0,
 				epochPassed: 0,
 				msg:         m,
-				sentSuccess: make(chan bool),
 			}
-			if len(c.window) == 0 {
-				c.window = append(c.window, newElem)
-				c.windowStart = m.SeqNum
-				c.windowUnAcked = 1
-				go c.WindowElemRountine(newElem)
-			} else if (m.SeqNum-c.window[0].seqNum < c.params.WindowSize) && (c.windowUnAcked < c.params.MaxUnackedMessages) {
-				c.window = append(c.window, newElem)
-				c.windowUnAcked += 1
-				go c.WindowElemRountine(newElem)
-			} else {
-				c.unsentBuffer = append(c.unsentBuffer, newElem)
-			}
+			c.newWindowElem <- newElem
 		case <-c.writeReq:
 			if c.alreadyDisconnect {
 				c.writeReturn <- false
 			} else {
 				c.writeReturn <- true
+			}
+		case <-c.connIDReq:
+			if c.connID >=0 {
+				c.connIDAnswer <- c.connID
 			}
 		}
 	}
@@ -467,12 +513,17 @@ func (c *client) ReadRoutine() {
 					switch msg.Type {
 					case MsgData:
 						if c.CheckCorrect(msg) {
+							ack := NewAck(msg.ConnID, msg.SeqNum)
+							c.ackToSend <- ack
 							c.incomeData <- msg
 						}
 						//fmt.Printf("incoming data received %v\n", msg.SeqNum)
 					case MsgAck:
 						//fmt.Printf("incoming acknowledge for request %v\n", msg.SeqNum)
-						c.incomeAck <- msg
+						if msg.SeqNum == 0 {
+							c.connMain <- msg.ConnID
+						}
+						c.newAckSeqNum <- msg.SeqNum
 					}
 					c.receiveSth <- true
 				}
